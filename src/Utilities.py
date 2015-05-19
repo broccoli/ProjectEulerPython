6c6b875b41808b930ccc9b08b66d14a27c64661e
broccoli <rldoan@hotmail.com>
2015-05-19 15:20:45 -0500

    initial commit

---
+++ src/Utilities.py
@@ -0,0 +1,34 @@
+
+from time import time
+
+class Timer:
+    
+    def start(self):
+        self.start_time = time()
+    def etime(self):
+        return time() - self.start_time
+
+
+def timewrap2(func):
+    def new_func(*args, **kwargs):
+        print "starting %s" % func.__name__
+        start_time = time()
+        func(*args, **kwargs)
+        end_time = time() - start_time
+        print "elapsed time: %f" % end_time
+        print
+    return new_func
+
+def timewrap(switch='off'):
+    def decorator(func):
+        def new_func(*args, **kwargs):
+            if switch == 'on':
+                print "starting %s" % func.__name__
+                start_time = time()
+            func(*args, **kwargs)
+            if switch == 'on':
+                end_time = time() - start_time
+                print "elapsed time: %f" % end_time
+                print
+        return new_func
+    return decorator
