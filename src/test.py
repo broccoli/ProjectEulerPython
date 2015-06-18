3985c328ebad6bee8880e032e7637f14964e7164
RDoan <rldoan@hotmail.com>
2015-06-18 18:58:30 -0500

    created unit test module

---
+++ src/test.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 18, 2015
+
+@author: richard
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
