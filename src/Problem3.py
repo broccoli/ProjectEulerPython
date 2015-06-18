dd66cca8cee1be85819b33dbcbccc188647ffb73
RDoan <rldoan@hotmail.com>
2015-06-17 19:32:04 -0500

    added solution for problem 3

--- src/Problem3.py
+++ src/Problem3.py
@@ -1,9 +1,42 @@
 
 
 def main():
-    
-    pass
+    """
+    Largest prime factor
+    Problem 3
+    The prime factors of 13195 are 5, 7, 13 and 29.
 
+    What is the largest prime factor of the number 600851475143 ?
+    """
+    num = 600851475143
+    
+    result = largest_prime_factor(num)
+    
+    print "largest prime: ", result
+    
+def largest_prime_factor(num):
+    prime_factors = []    
+    
+    while(True):
+        pf = smallest_prime_factor(num)
+        print pf
+        prime_factors.append(pf)
+        if pf == num:
+            break
+        num = num / pf
+    return num
+    
+def smallest_prime_factor(num):
+    """
+    return smallest prime factor, which may be itself
+    """
+    
+    if num % 2 == 0:
+        return 2
+    for x in xrange(3, num/3, 2):
+        if num % x == 0:
+            return x
+    return num
 
 if __name__ == '__main__':
     main()
