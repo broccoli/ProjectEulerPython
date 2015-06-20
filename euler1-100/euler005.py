4a74fc1585f85600abc2c99068a4f2d0e48538c8
RDoan <rldoan@hotmail.com>
2015-06-20 15:21:31 -0500

    added faster method

--- euler1-100/euler005.py
+++ euler1-100/euler005.py
@@ -6,6 +6,28 @@ What is the smallest positive number that is evenly divisible
 by all of the numbers from 1 to 20?'''
 
 import math
+import profile
+
+def smallest_multiple_b(num):
+    
+    
+    # get a list of primes less than num and multiply product by prime.
+    # For every time the prime squared, cubed, etc is lte the num,
+    # multiple product by the prime.  That takes care of 4, 8, 9, etc.
+    primes = []
+    prod = 1
+    for x in range(2, num + 1):
+        for p in primes:
+            if x % p == 0:
+                break
+        else:
+            primes.append(x)
+            prod = prod * x
+            exp = 2
+            while pow(x, exp) <= num:
+                prod = prod * x
+                exp += 1
+    return prod    
 
 def smallest_multiple(num):
     
@@ -15,11 +37,12 @@ def smallest_multiple(num):
     for x in primes:
         base = base * x
 
-    test_list = range(1, num + 1)
-    test_list.reverse()
-
+    test_list = range(num, 0, -1)
     
     test_num = base
+    
+    ctr = 0
+    
     while (True):
         
         stop = True
@@ -29,10 +52,13 @@ def smallest_multiple(num):
                 break
         if stop:
             break
-        test_num += base
-        
+        test_num += base    
+#     for x in test_list:
+#         while test_num % x != 0:
+#             test_num += base
         
     return test_num
+
 def get_primes_less_than(num):
     
     result = []
@@ -57,7 +83,20 @@ def is_prime(num):
             return False
     return True
 
+
+def test():
+    
+    reps = 10000
+    val = 30
+    for r in range(reps):
+        smallest_multiple(val)
+        smallest_multiple_b(val)
+        
+
 if __name__ == '__main__':
     
-    print smallest_multiple(20)
-
+    print smallest_multiple(10)
+    print smallest_multiple_b(10)
+    
+#     profile.run("test()")
+#     print get_primes_less_than(10)
